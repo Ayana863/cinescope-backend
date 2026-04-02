e872f23ae330bdc497661194fb8449cdf994efe5
Ayana863 <ayana863anna@gmail.com>
2026-04-02 20:15:07 +0530

    updated

--- index.js
+++ index.js
@@ -1,55 +1,3 @@
-// require('dotenv').config()
-// const cors=require('cors')
-// const express = require('express')
-// const connectDB = require('./DB/Connection')
-// const cookieParser = require("cookie-parser")
-// const authRouter = require('./Routes/authRouter')
-// const movieRouter=require('./Routes/movieRouter')
-// const userRoutes=require('./Routes/userRoutes')
-// const watchHistoryRoutes = require("./Routes/watchHistoryRoutes")
-// const ratingRoutes = require("./Routes/ratingRouter")
-
-// const app = express()
-
-
-// const allowedOrigins = [
-//   "http://localhost:5173",
-//   "https://mern-cine-project-v1ow.vercel.app"
-// ]
-
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin) return callback(null, true)
-
-//     if (allowedOrigins.includes(origin)) {
-//       callback(null, true)
-//     } else {
-//       callback(null, false)
-//     }
-//   },
-//   credentials: true,
-//   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
-// }))
-
-// app.use(express.json())
-// app.use(cookieParser())
-
-// app.use("/uploads", express.static("uploads"))
-// app.use('/auth', authRouter)
-// app.use('/admin', movieRouter)
-// app.use("/admin/users", userRoutes)
-// app.use("/watch-history", watchHistoryRoutes)
-// app.use("/ratings", ratingRoutes)
-
-
-
-// connectDB().then(() => {
-//   app.listen(5000, () => {
-//     console.log("Server running on port 5000")
-//   })
-// })
-
-
 require('dotenv').config()
 
 const cors = require('cors')
@@ -65,13 +13,13 @@ const ratingRoutes = require("./Routes/ratingRouter")
 
 const app = express()
 
-// ✅ Allowed origins
+// Allowed origins
 const allowedOrigins = [
   "http://localhost:5173",
   "https://mern-cine-project-v1ow.vercel.app/"
 ]
 
-// ✅ CORS setup
+// CORS setup
 app.use(cors({
   origin: function (origin, callback) {
     // Allow tools like Postman
@@ -87,21 +35,21 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
 }))
 
-// ✅ Middlewares
+// Middlewares
 app.use(express.json())
 app.use(cookieParser())
 
-// ✅ Static files
+//  Static files
 app.use("/uploads", express.static("uploads"))
 
-// ✅ Routes
+// Routes
 app.use('/auth', authRouter)
 app.use('/admin', movieRouter)
 app.use("/admin/users", userRoutes)
 app.use("/watch-history", watchHistoryRoutes)
 app.use("/ratings", ratingRoutes)
 
-// ✅ Connect DB and start server
+// Connect DB and start server
 connectDB().then(() => {
   app.listen(5000, () => {
     console.log("Server running on port 5000")
